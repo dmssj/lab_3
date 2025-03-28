057a7c00ce120f6eb0d2486172136d8654e76e9a
dmssj <letrey19@gmail.com>
2025-03-28 23:32:00 +0700

    добавлен determinant, inverse, getsubmatrix

--- lab3.cs
+++ lab3.cs
@@ -63,3 +63,54 @@ public SquareMatrix Multiply(SquareMatrix other)
     }
     return result;
 }
+
+public int Determinant()
+{
+    if (_size == 1) return _matrix[0][0];
+    if (_size == 2) return _matrix[0][0] * _matrix[1][1] - _matrix[0][1] * _matrix[1][0];
+
+    int result = 0;
+    for (int columnIndex = 0; columnIndex < _size; ++columnIndex)
+    {
+        SquareMatrix submatrix = GetSubmatrix(0, columnIndex);
+        if (columnIndex % 2 == 0) result += _matrix[0][columnIndex] * submatrix.Determinant();
+        else result -= _matrix[0][columnIndex] * submatrix.Determinant();
+    }
+    return result;
+}
+
+public SquareMatrix Inverse()
+{
+    int det = Determinant();
+    SquareMatrix result = new SquareMatrix(_size);
+    for (int rowIndex = 0; rowIndex < _size; ++rowIndex)
+    {
+        for (int columnIndex = 0; columnIndex < _size; ++columnIndex)
+        {
+            SquareMatrix submatrix = GetSubmatrix(rowIndex, columnIndex);
+            int sign = ((rowIndex + columnIndex) % 2 == 0) ? 1 : -1;
+            result._matrix[columnIndex][rowIndex] = sign * submatrix.Determinant() / det;
+        }
+    }
+    return result;
+}
+
+private SquareMatrix GetSubmatrix(int excludeRow, int excludeColumn)
+{
+    SquareMatrix submatrix = new SquareMatrix(_size - 1);
+    int subRowIndex = 0;
+    for (int rowIndex = 0; rowIndex < _size; ++rowIndex)
+    {
+        if (rowIndex == excludeRow) continue;
+
+        int subColumnIndex = 0;
+        for (int columnIndex = 0; columnIndex < _size; ++columnIndex)
+        {
+            if (columnIndex == excludeColumn) continue;
+            submatrix._matrix[subRowIndex][subColumnIndex] = _matrix[rowIndex][columnIndex];
+            ++subColumnIndex;
+        }
+        ++subRowIndex;
+    }
+    return submatrix;
+}
