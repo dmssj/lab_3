cc372ab10a21c6550b317aef871b2062205a08c0
dmssj <letrey19@gmail.com>
2025-03-28 23:27:38 +0700

    инициализация класса

---
+++ lab3.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+class SquareMatrix
+{
+    private List<List<int>> _matrix;
+    private int _size;
+
+    public SquareMatrix(int size)
+    {
+        _size = size;
+        _matrix = new List<List<int>>();
+
+        for (int rowIndex = 0; rowIndex < size; ++rowIndex)
+        {
+            _matrix.Add(new List<int>());
+            for (int columnIndex = 0; columnIndex < size; ++columnIndex)
+            {
+                _matrix[rowIndex].Add(new Random().Next(100));
+            }
+        }
+    }
+
+    public void PrintMatrix()
+    {
+        for (int rowIndex = 0; rowIndex < _size; ++rowIndex)
+        {
+            for (int columnIndex = 0; columnIndex < _size; ++columnIndex)
+            {
+                Console.Write(_matrix[rowIndex][columnIndex] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
